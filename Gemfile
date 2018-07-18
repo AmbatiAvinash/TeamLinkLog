90f29280051d9a963c1ff710f4e5961f0b78ba73
Atchyut Nagabhaiarava <atchyutnagabhairava@gmail.com>
2018-07-18 14:54:44 +0530

    Added pagination

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 #Add bootstrap to project
 gem 'bootstrap'
+#for pagination support
+gem 'will_paginate'
 gem 'jquery-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @links = Link.all.order(created_at: :desc)
+    @links = Link.paginate(:page => params[:page], :per_page => 6).order(created_at: :desc)
   end
 
   # GET /links/1
